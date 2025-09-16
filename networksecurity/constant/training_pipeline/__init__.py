4fed6fad8f2c8b96e96d2c6cba3650284ed1d2f2
suhasburge <suhasburge07@gmail.com>
2025-09-17 01:24:05 +0530

    Trainer Evaluation & mlflow Commited

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -15,6 +15,9 @@ TEST_FILE_NAME: str = 'test.csv'
 
 SCHEMA_FILE_PATH = os.path.join('data_schema','schema.yaml')
 
+SAVED_MODEL_DIR = os.path.join('data_schema','schema.yaml')
+MODEL_FILE_NAME = 'model.pkl'
+
 '''Data ingestion related constant start with DATA_INGESTION VAR NAME'''
 
 DATA_INGESTION_COLLECTION_NAME: str = 'NetworkData'
@@ -31,3 +34,34 @@ DATA_VALIDATION_VALID_DIR: str = 'validated'
 DATA_VALIDATION_INVALID_DIR: str = 'invalid'
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = 'drift_report'
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = 'report.yaml'
+PREPROCESSING_OBJECT_FILE_NAME = 'preprocessing.pkl'
+
+'''Data transformation related constant starts with DATA_TRANSFORMATION_VAR_NAME'''
+DATA_TRANSFORMATION_DIR_NAME: str = 'data_transformation'
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = 'transformed'
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = 'transformed_object'
+
+DATA_TRANSFORMATION_TRAIN_DIR: str = os.path.join(DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR, 'train')
+DATA_TRANSFORMATION_TEST_DIR: str = os.path.join(DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR, 'test')
+DATA_TRANSFORMATION_OBJECT_DIR: str = os.path.join(DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR)
+
+
+# KNN imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    'missing_values': np.nan,
+    'n_neighbors': 3,
+    'weights': 'uniform'
+}
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+"""
+Model Trainer related constant start with MODEL_TRAINER_VAR_NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVERFITTING_UNDERFITTING_THRESHOLD: float = 0.05
